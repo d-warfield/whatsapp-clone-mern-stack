f331fd8ad7b834398291a6acf43bbf5dec4d94be
Dennis Warfield <dpwarfield06@gmail.com>
2020-09-09 10:32:37 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
+import Chat from './Chat';
+import Pusher from 'pusher-js';
+import axios from "./axios";
+import { ChevronLeftOutlined } from '@material-ui/icons';
+// import { ContactSupportOutlined } from '@material-ui/icons';
 
 function App() {
+
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get('./messages/sync')
+      .then(response => {
+
+        setMessages(response.data)
+      })
+  }, [])
+
+  useEffect(() => {
+    const pusher = new Pusher('19540d82bde150d7b8fa', {
+      cluster: 'us2'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (newMessage) => {
+      alert(JSON.stringify(newMessage));
+      setMessages([...messages, newMessage])
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    }
+
+  }, [messages])
+
+  console.log(messages)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      <div className="app__body">
+        <Sidebar />
+        <Chat />
+      </div>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAcMZoEi--THarc_mvfHCpWy25iIqNMB1U",
+  authDomain: "whatsapp-mern-8c553.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-8c553.firebaseio.com",
+  projectId: "whatsapp-mern-8c553",
+  storageBucket: "whatsapp-mern-8c553.appspot.com",
+  messagingSenderId: "1063826844554",
+  appId: "1:1063826844554:web:60b8b9bb260676def4b673",
+  measurementId: "G-RFWTVPTCFY"
+};
